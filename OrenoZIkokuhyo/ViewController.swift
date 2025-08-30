a55bae3cdf954b1538bd25dc438fb09f8f9cb551
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-30 14:13:07 +0900

    Initial Commit

---
+++ OrenoZIkokuhyo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrenoZIkokuhyo
+//
+//  Created by ynagaro on 2025/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrenoZIkokuhyoTests/OrenoZIkokuhyoTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrenoZIkokuhyoTests.swift
+//  OrenoZIkokuhyoTests
+//
+//  Created by ynagaro on 2025/08/30.
+//
+
+import Testing
+@testable import OrenoZIkokuhyo
+
+struct OrenoZIkokuhyoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
