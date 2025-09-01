eee93331764b34d965253d0973841a7f0fec28e2
yasuonagaro <yasuo.nagaro@gmail.com>
2025-09-02 00:31:43 +0900

    ナビタイムのAPIに変更

--- OrenoJikokuhyo/TokyuStationData.swift
+++
@@ -1,7 +0,0 @@
-struct TokyuStationData {
-    static let stations: [Station] = [
-        Station(name: "渋谷", id: "odpt.Station:Tokyu.Toyoko.Shibuya", directionID: "odpt.RailDirection:Tokyu.Yokohama"),
-        Station(name: "中目黒", id: "odpt.Station:Tokyu.Toyoko.NakaMeguro", directionID: "odpt.RailDirection:Tokyu.Yokohama"),
-        // ... 他の東急線の駅をすべて定義 ...
-    ]
-}
