25843356abd46c7ccc3a887c0b3fd7107bc4219f
yasuonagaro <yasuo.nagaro@gmail.com>
2025-09-02 02:08:28 +0900

    APIを変更

--- OrenoJikokuhyo/Station.swift
+++
@@ -1,6 +0,0 @@
-// Station.swift - アプリ内で使う駅情報のモデル
-struct Station {
-    let name: String // 例: "渋谷"
-    let id: String   // 例: "odpt.Station:Tokyu.Toyoko.Shibuya"
-    let directionID: String // 例: "odpt.RailDirection:Tokyu.Yokohama"
-}
